d31d6d97611949ccc04b7c3ce905b1155a8153a7
Taina Hilliaho <taina.hilliaho@gmail.com>
2025-01-10 18:11:32 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Kumpulan Speksi 2025</h1>
+    </>
+  );
+}
+
+export default App;
