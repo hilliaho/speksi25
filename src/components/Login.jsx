365e9dfb2105da90d67bdfeb1924b78adf71bdde
Taina Hilliaho <taina.hilliaho@gmail.com>
2025-02-16 09:51:26 +0200

    Add login for staging page for feedback

---
+++ src/components/Login.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useState } from "react";
+
+const Login = ({ setAuthored }) => {
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (password === "SalainenSalasanaSpeksinProduktionSivulle") {
+      setAuthored(true);
+    } else {
+      setError("väärä salasana :(");
+    }
+  };
+
+  return (
+    <div className="login-display">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input type="submit" />
+      </form>
+      {error}
+    </div>
+  );
+};
+
+export default Login;
